c7dc15550beb8d7fdeaeaef5caaaa493a775fec2
KarinRosen <76880796+KarinRosen@users.noreply.github.com>
2021-11-29 04:06:39 +0200

    Add files via upload

---
+++ Keeperveebileht/avaleht.js
@@ -0,0 +1,7 @@
+    <script type="text/javascript">
+    function bronnima()
+    {
+    var url = "C:\Users\karin\Downloads\Veebilehe tegemine\Keeperveebileht\bronn.html";
+    window.location(url);
+    }
+    </script>
