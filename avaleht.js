fc8bd6eee75b5e0ee8a2917d292c42e9f62eb933
KarinRosen <76880796+KarinRosen@users.noreply.github.com>
2021-11-29 18:13:07 +0200

    Add files via upload

--- avaleht.js
+++ avaleht.js
@@ -1,6 +1,8 @@
+//funktsioon avab bronnilehe
 function avabronn() {
-  window.open("bronn.html").focus();
+  window.open("bronn.html");
 }
+//funktsioon avab testilehe
 function avatest() {
-  window.open("test.html").focus();
+  window.open("test.html");
 }
--- koolitused.js
+++ koolitused.js
@@ -1,56 +1,56 @@
 
-// Get the modal
+// milline dialoogiaken
 var modal1 = document.getElementById("myModal1");
 
-// Get the button that opens the modal
+// milline nupp avab akna
 var btn1 = document.getElementById("nupp1");
 
 
-// Get the <span> element that closes the modal
+// milline ristike sulgeb akna
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
+// kui nuppu klikata aken avaneb 
 btn1.onclick = function() {
   modal1.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// kui ristikest klikata aken sulgub
 span.onclick = function() {
   modal1.style.display = "none";
 }
 
 
 
-// Get the modal
+// milline dialoogiaken
 var modal2 = document.getElementById("myModal2");
 
-// Get the button that opens the modal
+// milline nupp avab akna
 var btn2 = document.getElementById("nupp2");
 
 
-// Get the <span> element that closes the modal
+// milline ristike sulgeb akna
 var span2 = document.getElementsByClassName("close2")[0];
 
-// When the user clicks the button, open the modal 
+// kui nuppu klikata aken avaneb
 btn2.onclick = function() {
   modal2.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// kui ristikest klikata aken sulgub
 span2.onclick = function() {
   modal2.style.display = "none";
 }
 
 
 
-// Get the modal
+// milline dialoogiaken
 var modal3 = document.getElementById("myModal3");
 
-// Get the button that opens the modal
+// milline nupp avab akna
 var btn3 = document.getElementById("nupp3");
 
 
-// Get the <span> element that closes the modal
+// milline ristike sulgeb akna
 var span3 = document.getElementsByClassName("close3")[0];
 
 // When the user clicks the button, open the modal 
@@ -58,114 +58,112 @@ btn3.onclick = function() {
   modal3.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// kui ristikest klikata aken sulgub
 span3.onclick = function() {
   modal3.style.display = "none";
 }
 
 
-// Get the modal
+// milline dialoogiaken
 var modal4 = document.getElementById("myModal4");
 
-// Get the button that opens the modal
+// milline nupp avab akna
 var btn4 = document.getElementById("nupp4");
 
-
-// Get the <span> element that closes the modal
+// milline ristike sulgeb akna
 var span4 = document.getElementsByClassName("close4")[0];
 
-// When the user clicks the button, open the modal 
+// kui nuppu klikata aken avaneb
 btn4.onclick = function() {
   modal4.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// kui ristikest klikata aken sulgub
 span4.onclick = function() {
   modal4.style.display = "none";
 }
 
-// Get the modal
+// milline dialoogiaken
 var modal5 = document.getElementById("myModal5");
 
-// Get the button that opens the modal
+// milline nupp avab akna
 var btn5 = document.getElementById("nupp5");
 
 
-// Get the <span> element that closes the modal
+// milline ristike sulgeb akna
 var span5 = document.getElementsByClassName("close5")[0];
 
-// When the user clicks the button, open the modal 
+// kui nuppu klikata aken avaneb
 btn5.onclick = function() {
   modal5.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// kui ristikest klikata aken sulgub
 span5.onclick = function() {
   modal5.style.display = "none";
 }
 
-
-// Get the modal
+// milline dialoogiaken
 var modal6 = document.getElementById("myModal6");
 
-// Get the button that opens the modal
+// milline nupp avab akna
 var btn6 = document.getElementById("nupp6");
 
 
-// Get the <span> element that closes the modal
+// milline ristike sulgeb akna
 var span6 = document.getElementsByClassName("close6")[0];
 
-// When the user clicks the button, open the modal 
+// kui nuppu klikata aken avaneb
 btn6.onclick = function() {
   modal6.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// kui ristikest klikata aken sulgub
 span6.onclick = function() {
   modal6.style.display = "none";
 }
 
 
 
-// Get the modal
+// milline dialoogiaken
 var modal7 = document.getElementById("myModal7");
 
-// Get the button that opens the modal
+// milline nupp avab akna
 var btn7 = document.getElementById("nupp7");
 
 
-// Get the <span> element that closes the modal
+// milline ristike sulgeb akna
 var span7 = document.getElementsByClassName("close7")[0];
 
-// When the user clicks the button, open the modal 
+// kui nuppu klikata aken avaneb 
 btn7.onclick = function() {
   modal7.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// kui ristikest klikata aken sulgub
 span7.onclick = function() {
   modal7.style.display = "none";
 }
 
 
 
-// Get the modal
+// milline dialoogiaken
 var modal8 = document.getElementById("myModal8");
 
-// Get the button that opens the modal
+// milline nupp avab akna
 var btn8 = document.getElementById("nupp8");
 
-
-// Get the <span> element that closes the modal
+// milline ristike sulgeb akna
 var span8 = document.getElementsByClassName("close8")[0];
 
-// When the user clicks the button, open the modal 
+// kui nuppu klikata aken avaneb
 btn8.onclick = function() {
   modal8.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// kui ristikest klikata aken sulgub
 span8.onclick = function() {
   modal8.style.display = "none";
 }
 
+// viide https://www.quanzhanketang.com/howto/tryhow_css_modal.html?filename=tryhow_css_modal
