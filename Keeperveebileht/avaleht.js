1b7ea6a7c886680c2863ba9ac9e8fad6f307f66a
KarinRosen <76880796+KarinRosen@users.noreply.github.com>
2021-11-29 04:07:26 +0200

    Delete Keeperveebileht directory

--- Keeperveebileht/avaleht.js
+++
@@ -1,7 +0,0 @@
-    <script type="text/javascript">
-    function bronnima()
-    {
-    var url = "C:\Users\karin\Downloads\Veebilehe tegemine\Keeperveebileht\bronn.html";
-    window.location(url);
-    }
-    </script>
